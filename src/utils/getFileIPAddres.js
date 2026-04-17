5458b030235a0d728a716cf4032871217657d224
niranjan-SFT-Tech <service@shyamfuture.dev>
2026-04-17 17:22:58 +0530

    fix multer upload

--- src/utils/getFileIPAddres.js
+++ src/utils/getFileIPAddres.js
@@ -1,22 +1,31 @@
-import url  from 'node:url';
-import path from 'node:path'; 
+// import url  from 'node:url';
+// import path from 'node:path'; 
 import { upload_dir } from '../configs/index.js';
+import path from "path";
+import url from "url";
 
 // Get baseURL
 export const getBaseURL = (protocal, host) => {
   return `${protocal}://${host}`;
 }
 
+export const filePathToHttpURL = (baseURL, filePath) => {
+  const mediaRoot = "/var/www/html/producthrmsmanagement/media";
+  // Convert FS path → relative HTTP path
+  const relativePath = filePath.replace(mediaRoot, "").replace(/\\/g, "/");
+  return `${baseURL}/media${relativePath}`;
+};
+//old code
 // Convert file path to HTTP URL with IP
-export const filePathToHttpURL = (baseURL, filePath, staticRoute = upload_dir) => {
-  console.log(upload_dir,"--Upload DIr")   
-  const fileName = path.basename(filePath);  
-  const httpURL = new url.URL(`${baseURL}`); 
-  httpURL.pathname = `${staticRoute}/${fileName}`;  
-  console.log(httpURL.href,"httpURL.href")
-  console.log(httpURL.pathname,"httpURL.pathname")
-  return httpURL.href;
-}
+// export const filePathToHttpURL = (baseURL, filePath, staticRoute = upload_dir) => {
+//   console.log(upload_dir,"--Upload DIr")   
+//   const fileName = path.basename(filePath);  
+//   const httpURL = new url.URL(`${baseURL}`); 
+//   httpURL.pathname = `${staticRoute}/${fileName}`;  
+//   console.log(httpURL.href,"httpURL.href")
+//   console.log(httpURL.pathname,"httpURL.pathname")
+//   return httpURL.href;
+// }
 
 // // // Example usage
 // const file = { path: 'file:///C:/var/www/html/tkg/producthrmstkg/media/uploads/resume-sample-1770285899260-704969235.pdf' };
